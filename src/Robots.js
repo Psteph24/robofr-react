f070a0d8816f5a33093a1b93bff8fdfa4e4c9fd8
Psteph24 <psteph24@icloud.com>
2023-03-29 19:37:36 +0300

    react app

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Marius Manole',
+    username: 'Mars',
+    email: 'mariusmanole@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Radu Postescu',
+    username: 'Ratt',
+    email: 'radupostescu@gmail.com'
+  },
+  {
+    id: 3,
+    name: 'Dan Dan',
+    username: 'DD',
+    email: 'dandandandan@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Patricia Tanase',
+    username: 'Pati45',
+    email: 'pati.tanase@gmail.com'
+  },
+  {
+    id: 5,
+    name: 'Clara Juvila',
+    username: 'Clr12',
+    email: 'claraaa12@gmail.com'
+  },
+  {
+    id: 6,
+    name: 'Denis Nebunu',
+    username: 'DnEbunu',
+    email: 'denisnebununebunu@gmail.com'
+  },
+  {
+    id: 7,
+    name: 'Andreea Varga',
+    username: 'AVG1',
+    email: 'andreea.e.greu16@gmail.com'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Eriksson',
+    username: 'Nikl8',
+    email: 'niku.niku@gmail.com'
+  },
+  {
+    id: 9,
+    name: 'Kostas Ioannidis',
+    username: 'cKostas',
+    email: 'kostasioannidis.gmail.com'
+  },
+  {
+    id: 10,
+    name: 'Stefan cel Mare',
+    username: 'moldoveanu33',
+    email: 'domnitorucica@gmail.com'
+  }
+];
